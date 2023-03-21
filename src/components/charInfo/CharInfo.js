654972cab7db87131db026f6ffa632d8832357eb
nyan92213 <ivanbezrukov92213@gmail.com>
2023-03-21 20:55:16 +0300

    added errorBoundary in most important components

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -36,7 +36,6 @@ class CharInfo extends Component {
             .getCharacter(charId)
             .then(this.onCharLoaded)
             .catch(this.onError)
-        this.foo.bar = 0
     }
 
     onCharLoaded = (char) => {
