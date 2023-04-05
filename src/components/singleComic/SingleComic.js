c21d2a74fd44885fe3732e9695c910dbc3eebf4c
nyan92213 <ivanbezrukov92213@gmail.com>
2023-04-05 18:32:05 +0300

    added routing v5

--- src/components/singleComic/SingleComic.js
+++ src/components/singleComic/SingleComic.js
@@ -1,20 +1,42 @@
-import './singleComic.scss';
-import xMen from '../../resources/img/x-men.png';
+import "./singleComic.scss";
+import useMarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import PropTypes from "prop-types";
 
 const SingleComic = () => {
-    return (
-        <div className="single-comic">
-            <img src={xMen} alt="x-men" className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">X-Men: Days of Future Past</h2>
-                <p className="single-comic__descr">Re-live the legendary first journey into the dystopian future of 2013 - where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops himself...and a demon for Christmas!?</p>
-                <p className="single-comic__descr">144 pages</p>
-                <p className="single-comic__descr">Language: en-us</p>
-                <div className="single-comic__price">9.99$</div>
-            </div>
-            <a href="#" className="single-comic__back">Back to all</a>
-        </div>
-    )
-}
+  const [comicsList, setComicsList] = useState([]);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(210);
+  const [charEnded, setCharEnded] = useState(false);
+
+  const { loading, error, getAllCharacters } = useMarvelService();
+
+  useEffect(() => {
+    // т.к. useEffect вызывается после конструирования страницы, функцию в нем можно вызывать до объявления по коду
+    onRequest(offset, true);
+  }, []);
+
+  const items = renderItems(charList);
+
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading && !newItemLoading ? <Spinner /> : null;
+
+  return (
+    <div className="comics__list">
+      {errorMessage}
+      {spinner}
+      {items}
+      <button
+        className="button button__main button__long"
+        disabled={newItemLoading}
+        style={{ display: charEnded ? "none" : "block" }}
+        onClick={() => onRequest(offset)}
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 export default SingleComic;
