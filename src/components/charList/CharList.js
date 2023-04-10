8c1f534c60ada577504537a201d7feb88c7ba26b
nyan92213 <ivanbezrukov92213@gmail.com>
2023-04-10 10:11:38 +0300

    updated comicsList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -5,13 +5,32 @@ import Spinner from "../spinner/spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import PropTypes from "prop-types";
 
+const setContent = (process, Component, newItemLoading) => {
+  switch (process) {
+    case "waiting":
+      return <Spinner />;
+      break;
+    case "loading":
+      return newItemLoading ? <Component /> : <Spinner />;
+      break;
+    case "confirmed":
+      return <Component />;
+      break;
+    case "error":
+      return <ErrorMessage />;
+    default:
+      throw new Error("Unexpected process state");
+  }
+};
+
 const CharList = (props) => {
   const [charList, setCharList] = useState([]);
   const [newItemLoading, setNewItemLoading] = useState(false);
   const [offset, setOffset] = useState(210);
   const [charEnded, setCharEnded] = useState(false);
 
-  const { loading, error, getAllCharacters } = useMarvelService();
+  const { loading, error, getAllCharacters, process, setProcess } =
+    useMarvelService();
 
   useEffect(() => {
     // т.к. useEffect вызывается после конструирования страницы, функцию в нем можно вызывать до объявления по коду
@@ -20,7 +39,9 @@ const CharList = (props) => {
 
   const onRequest = (offset, initial) => {
     initial ? setNewItemLoading(false) : setNewItemLoading(true);
-    getAllCharacters(offset).then(onCharListLoaded);
+    getAllCharacters(offset)
+      .then(onCharListLoaded)
+      .then(() => setProcess("confirmed"));
   };
 
   const onCharListLoaded = (newCharList) => {
@@ -81,11 +102,6 @@ const CharList = (props) => {
     return <ul className="char__grid">{items}</ul>;
   }
 
-  const items = renderItems(charList);
-
-  const errorMessage = error ? <ErrorMessage /> : null;
-  const spinner = loading && !newItemLoading ? <Spinner /> : null;
-
   // if (loading) {
   //   import("./someFunc")
   //     .then((obj) => obj.default())
@@ -94,9 +110,7 @@ const CharList = (props) => {
 
   return (
     <div className="char__list">
-      {errorMessage}
-      {spinner}
-      {items}
+      {setContent(process, () => renderItems(charList), newItemLoading)}
       <button
         className="button button__main button__long"
         disabled={newItemLoading}
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,8 @@
 import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
-  const { loading, request, error, clearError } = useHttp();
+  const { loading, request, error, clearError, process, setProcess } =
+    useHttp();
 
   const _apiBase = "https://gateway.marvel.com:443/v1/public/";
   const _apiKey = "apikey=49cf50974e36d6fbc26c8729936c2e12";
@@ -76,6 +77,8 @@ const useMarvelService = () => {
     getAllComics,
     getComic,
     getCharacterByName,
+    process,
+    setProcess,
   };
 };
 
